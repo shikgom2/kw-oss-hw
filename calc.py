49da424fbad902ebcb8500dfee1eda4764d8145b
rusidian <127274673+rusidian@users.noreply.github.com>
2023-04-10 09:52:33 +0900

    덧셈 뺄셈 곱셈 나누기 추가

--- calc.py
+++ calc.py
@@ -2,13 +2,19 @@
 # return sum
 def plus(x, y):
     return x+y
+def mi(x,y):
+    return x-y
+def re(x,y):
+    return x*y
+def re2(x,y):
+    return x/y
 
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:
-        print("0: exit, 1: plus")
+    while check >= 1:        
+        print("0: exit, 1: plus, 2: minus, 3:times, 4:divied ")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -16,10 +22,39 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
-        elif check > 1:
+        elif check > 4:
             print("Unsupported")
         else:
             print("Thank you")
+
+        if check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", mi(x,y))
+        elif check > 4:
+            print("Unsupported")
+
+        if check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", re(x,y))
+        elif check > 4:
+            print("Unsupported")
+
+
+        if check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", re2(x,y))
+        elif check > 4:
+            print("Unsupported")
+
 
 if __name__ == "__main__":
     main()
